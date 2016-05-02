3e129d855d12eb75ea21a5a2725e7a1e4eb42b54
profalex <prof.alex.salgado@gmail.com>
2016-05-02 13:25:08 +0000

    sqrt

---
+++ raiz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num = 49;
+    float raiz;
+    
+    raiz = sqrt(num);
+    
+    printf("\n Raiz Quadrada = %.2f", raiz);
+    
+    return 0;
+    
+    
+}
